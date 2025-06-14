0e405d5204de3da9e0cd8aab5073173fb9a93c31
Quang Duy <126586796+QuangDuy212@users.noreply.github.com>
2025-06-14 15:07:13 +0700

    feat: added-week-and-month-revenue

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -6,16 +6,24 @@ import {
   updateOrderToDelivered,
   getMyOrders,
   getOrders,
+  getCustomerOrders,
+  getRevenueWeek,
+  getRevenueMonth,
+  getRevenue7Days,
 } from '../controllers/orderController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', createOrder);
-router.get('/',  getOrders);
+router.get('/', getOrders);
 router.get('/myorders', getMyOrders);
-router.get('/:id',  getOrderById);
-router.put('/:id/pay',  updateOrderToPaid);
+router.get('/:id', getOrderById);
+router.put('/:id/pay', updateOrderToPaid);
 router.put('/:id/deliver', updateOrderToDelivered);
+
+router.get('/revenue/week', getRevenueWeek); // Route doanh thu tuần
+router.get('/revenue/month', getRevenueMonth); // Route doanh thu tháng
+router.get('/customer/:customerId', getCustomerOrders); // Route đơn hàng khách hàng
+router.get('/revenue/7days', getRevenue7Days); // Route doanh thu 7 ngày qua
 
 export default router; 
